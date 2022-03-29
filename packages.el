699c07c4617687ff09b0a49be9a690108f51759f
William Emfinger <waemfinger@gmail.com>
2022-03-29 14:13:14 -0500

    added docstr package.

--- packages.el
+++ packages.el
@@ -11,6 +11,9 @@
 
 (package! blamer)
 
+(package! docstr
+  :recipe (:host github :repo "jcs-elpa/docstr"))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
