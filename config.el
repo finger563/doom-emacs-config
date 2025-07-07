0570a08158321ab201ac40995380201c9752e522
William Emfinger <waemfinger@gmail.com>
2025-07-07 11:06:29 -0500

    minor update for copilot by default in markdown and org files

--- config.el
+++ config.el
@@ -162,7 +162,19 @@
               ("M-n" . 'copilot-next-completion)
               ("M-p" . 'copilot-previous-completion)
               ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
+              ("C-<tab>" . 'copilot-accept-completion-by-word))
+
+  :config
+  (add-to-list 'copilot-major-mode-alist '("org" . "plaintext"))
+  (add-to-list 'copilot-major-mode-alist '("org-roam" . "plaintext"))
+  (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode 2))
+  (add-to-list 'copilot-indentation-alist '(text-mode 2))
+  (add-to-list 'copilot-indentation-alist '(closure-mode 2))
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2)))
+
+(add-hook 'org-mode-hook 'copilot-mode)
+(add-hook 'markdown-mode-hook 'copilot-mode)
 
 (defun cae-copilot-clear-overlay-h ()
  "Like `copilot-clear-overlay', but returns `t' if the overlay was visible."
